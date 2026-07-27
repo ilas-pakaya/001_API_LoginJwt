3c8e4d63d544926b44b9b23c128cc208b73356fc
MOJI <apple@apples-MacBook-Air.local>
2026-07-27 09:58:59 +0700

    add code to user.js

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require("sequelize");  
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return User;
+};
