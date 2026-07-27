b85a3a177fa889ce20bac1c1d8f1f4de1e3f3e8a
MOJI <apple@apples-MacBook-Air.local>
2026-07-27 10:40:16 +0700

    add const token to authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,3 +9,11 @@ const authMiddleware = (req, res, next) => {
                 message: "Authorization token tidak ditemukan."
             });
         }
+
+        const token = authHeader.split(' ')[1];
+
+        if (!token) {
+            return res.status(400).json({
+                message: "Format token tidak valid."
+            });
+        }
