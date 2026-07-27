ead6d5438773fd91a0ade315456cfad959117d08
MOJI <apple@apples-MacBook-Air.local>
2026-07-27 10:07:30 +0700

    add function register

--- controller/userController.js
+++ controller/userController.js
@@ -2,3 +2,9 @@ const db = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const user = db.user;
+
+async function register(req, res) {
+    try {
+        const
+    }
