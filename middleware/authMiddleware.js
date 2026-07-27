85fbb1628a3f9e95a17c7fb2a002590849ac23ed
MOJI <apple@apples-MacBook-Air.local>
2026-07-27 10:39:04 +0700

    add const authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,2 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+const authMiddleware = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(400).json({
+                message: "Authorization token tidak ditemukan."
+            });
+        }
