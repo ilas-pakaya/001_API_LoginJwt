127350547b34e8e861bbdb7d7f7ee25cd14aaab7
MOJI <apple@apples-MacBook-Air.local>
2026-07-27 10:14:14 +0700

    add hashedPassword

--- controller/userController.js
+++ controller/userController.js
@@ -21,6 +21,20 @@ async function register(req, res) {
                 message: "Email sudah terdaftar."
             });
         }
-        
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            email,
+            password: hashedPassword
+        });
+        return res.status(201).json({
+            message: "Registrasi berhasil.",
+            data: {
+                id: user.id,
+                email: user.email
+            }
+        });
+    }
         
     
